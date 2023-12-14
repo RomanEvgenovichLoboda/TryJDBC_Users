fa6f468942277d07e7d7daf3d67e5f8831e56111
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2023-12-14 14:39:13 +0200

    Initial commit

---
+++ src/main/java/SpringJDBC/TryJDBC/TryJdbcApplication.java
@@ -0,0 +1,14 @@
+package SpringJDBC.TryJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryJdbcApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TryJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringJDBC/TryJDBC/TryJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringJDBC.TryJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
